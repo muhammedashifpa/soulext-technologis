4989a7039c36914d2d771ffe797ffeab406a5197
Ashif Muhammed <ashifmohammed231@gmail.com>
2026-02-25 00:15:32 +0530

    refactor: Consolidate theme palette and configuration into a single `theme.js` file and update import.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import theme from "@/theme";
+import theme from "@/theme/theme";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Box } from "@mui/material";
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,13 +1,37 @@
 "use client";
 import { Inter } from "next/font/google";
 import { createTheme } from "@mui/material/styles";
-import palette from "./theme/palette";
 
 const inter = Inter({
   weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
   display: "swap",
 });
+
+const palette = {
+  mode: "light",
+  primary: {
+    main: "#5d36af",
+    light: "#ede7f6",
+  },
+  secondary: {
+    main: "#e07b39",
+  },
+  success: {
+    main: "#27ae60",
+  },
+  error: {
+    main: "#eb5757",
+  },
+  background: {
+    default: "#F3F3F3",
+    paper: "#ffffff",
+  },
+  text: {
+    primary: "#222222",
+    secondary: "#666666",
+  },
+};
 
 const theme = createTheme({
   cssVariables: true,
